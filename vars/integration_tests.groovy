1391b5b4112deb13aa2e81d934b42ac89253ec1c
Rahul <42219389+rahulpatidar0191@users.noreply.github.com>
2021-05-31 13:04:34 -0700

    Update integration_tests.groovy

--- vars/integration_tests.groovy
+++ vars/integration_tests.groovy
@@ -1,8 +1,8 @@
 def call(directory) {
  	dir(directory){	 
 	   docker.image('circleci/node:12.13-browsers').inside{
-		sh 'yarn install'
-		sh 'yarn cypress install'
+		//sh 'yarn install'
+		echo 'yarn cypress install'
 		// sh ' yarn cy:run'
 		 }
 	   }
