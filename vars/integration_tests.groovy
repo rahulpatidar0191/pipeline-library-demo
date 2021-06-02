48004dd77c15b3b2c674df41cc8f353903e802b0
Rahul <42219389+rahulpatidar0191@users.noreply.github.com>
2021-06-02 14:05:35 -0700

    Update integration_tests.groovy

--- vars/integration_tests.groovy
+++ vars/integration_tests.groovy
@@ -2,7 +2,8 @@ def call(directory) {
  	dir(directory){	 
 	   docker.image('circleci/node:12.13-browsers').inside{
 		sh 'yarn install'
-		sh 'yarn cy:run --record --key 5edfee75-c874-466c-b96c-3a457f8eca5c'
+		//sh 'yarn cy:run --record --key 5edfee75-c874-466c-b96c-3a457f8eca5c'
+		sh 'yarn e2e:record:parallel'   
 		 }
 	   }
 }
