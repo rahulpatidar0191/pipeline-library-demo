754d0257428ccd39fb92a827076e0f00e1bbd64e
Rahul <42219389+rahulpatidar0191@users.noreply.github.com>
2021-06-04 10:18:34 -0700

    Update integration_tests.groovy

--- vars/integration_tests.groovy
+++ vars/integration_tests.groovy
@@ -1,8 +1,17 @@
-def call(directory) {
-  stage('tests') {
-     dir(directory) {
-       echo directory
-     }
-  }
-}
+// def call(directory) {
+//   stage('tests') {
+//      dir(directory) {
+//        echo directory
+//      }
+//   }
+// }
 
+def call(directory) {
+   stages {
+     stage('test') {
+        steps {
+          echo directory
+        }
+     }
+   }
+}
