7ad9e2a73b812ce06314f23c931a511fb77270cb
Rahul <42219389+rahulpatidar0191@users.noreply.github.com>
2021-06-04 10:12:32 -0700

    Update integration_tests.groovy

--- vars/integration_tests.groovy
+++ vars/integration_tests.groovy
@@ -1,12 +1,15 @@
 def call(directory) {
-  dir(directory) {
-             //sh 'yarn config set registry https://registry.yarnpkg.com/'
-            // sh 'yarn config set registry https://registry.npmjs.org/'
-            // sh 'yarn cache clean'
-             sh 'yarn install --network-concurrency 1'
-             sh 'yarn cypress install'
-             echo "Running build ${env.BUILD_ID}"
-              //sh 'yarn e2e:record:parallel'
+  stage('tests') {
+     dir(directory) {
+           // sh 'yarn config set registry https://registry.yarnpkg.com/'
+           // sh 'yarn config set registry https://registry.npmjs.org/'
+           // sh 'yarn cache clean'
+            sh 'yarn install --network-concurrency 1'
+            sh 'yarn cypress install'
+            echo "Running build ${env.BUILD_ID}"
+            //sh 'yarn e2e:record:parallel'
+       
+     }
   }
 }
 
