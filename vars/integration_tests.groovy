1a8009aede97f7e9585cb00eb271536fd1ba4913
Rahul <42219389+rahulpatidar0191@users.noreply.github.com>
2021-06-08 14:37:17 -0700

    Update integration_tests.groovy

--- vars/integration_tests.groovy
+++ vars/integration_tests.groovy
@@ -1,13 +1,14 @@
-def call(app_name) {
+def call(app_name,npm-token) {
   // node {
   stage('tests') {
-    environment {
-    NPM_TOKEN = credentials('npm-token')
-    CI = 'true'
-    PUBLIC_URL = '%PUBLIC_URL%'
-   }
+//     environment {
+//     NPM_TOKEN = credentials('npm-token')
+//     CI = 'true'
+//     PUBLIC_URL = '%PUBLIC_URL%'
+//    }
      dir(app_name) {
        docker.image('node:12').inside {
+         withCredentials([string(credentialsId: npm-token, usernameVariable: 'NPM_TOKEN')]) {
             echo NPM_TOKEN
              //echo app_name
             // sh 'yarn install'
