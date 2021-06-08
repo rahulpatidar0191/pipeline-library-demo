6b631db7484e927873a56f551aded18565387005
Rahul <42219389+rahulpatidar0191@users.noreply.github.com>
2021-06-08 14:43:02 -0700

    Update integration_tests.groovy

--- vars/integration_tests.groovy
+++ vars/integration_tests.groovy
@@ -1,22 +1,39 @@
 def call(app_name,npm_token) {
-  // node {
   stage('tests') {
-//     environment {
-//     NPM_TOKEN = credentials('npm-token')
-//     CI = 'true'
-//     PUBLIC_URL = '%PUBLIC_URL%'
-//    }
      dir(app_name) {
        docker.image('node:12').inside {
          withCredentials([string(credentialsId: npm_token, usernameVariable: 'NPM_TOKEN')]) {
             echo NPM_TOKEN
-             //echo app_name
-            // sh 'yarn install'
          }
         }
      }
-  //}
-}     
+  }
+}  
+
+
+
+
+
+
+// def call(app_name,npm_token) {
+//   // node {
+//   stage('tests') {
+// //     environment {
+// //     NPM_TOKEN = credentials('npm-token')
+// //     CI = 'true'
+// //     PUBLIC_URL = '%PUBLIC_URL%'
+// //    }
+//      dir(app_name) {
+//        docker.image('node:12').inside {
+//          withCredentials([string(credentialsId: npm_token, usernameVariable: 'NPM_TOKEN')]) {
+//             echo NPM_TOKEN
+//              //echo app_name
+//             // sh 'yarn install'
+//          }
+//         }
+//      }
+//   //}
+// }     
 
 
 // def call(directory) {
