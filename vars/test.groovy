3df76a3a3e72f4bd23d52689cf176192d570547e
Rahul <42219389+rahulpatidar0191@users.noreply.github.com>
2021-06-08 21:18:24 -0700

    Update test.groovy

--- vars/test.groovy
+++ vars/test.groovy
@@ -1,130 +1,29 @@
-// def call(Map pipelineParams) {
-//     pipeline {
-//       agent {
-//         docker {
-//           image 'node:12'
-//         }
-//       }
-//       environment {
-//         NPM_TOKEN = credentials('npm-token')
-//         CI = 'true'
-//         PUBLIC_URL = '%PUBLIC_URL%'
-//       }
-//       stages {
-//         stage('Install Dependencies') {
-//           steps {
-//             /* groovylint-disable-next-line GStringExpressionWithinString */
-//             sh 'cd client; echo "//registry.npmjs.org/:_authToken=\${NPM_TOKEN}" > .npmrc'
-//             sh 'cd client; yarn install'
-//           }
-//         }
-//         stage('Lint') {
-//           steps {
-//             sh 'cd client; yarn lint'
-//           }
-//         }
-//         stage('Test') {
-//           steps {
-//             sh 'cd client; yarn test'
-//           }
-//         }
-//         stage('Build') {
-//           steps {
-//             sh 'cd client; yarn build'
-//           }
-//         }
-//       }
-//     }
-// }
 
+def server(Map pipelineParams) {
+    node {
+      stage('Checkout'){
+        checkout scm
+      }
 
+      stage('Build Docker') {
+          echo 'Building docker image ...'
+          def registry = 'docker.satel.ca'
+          env.REGISTRY = 'docker.satel.ca'
+          def credentials = 'satel_bigbilly_registry'
+          if (env.TAG_NAME != null) {
+            registry = pipelineParams.registry
+            env.REGISTRY = pipelineParams.registry
+            credentials = pipelineParams.credentials
+          }
+          docker.withRegistry('https://' + registry, credentials) {
 
-
-
-
-def client(Map pipelineParams) {
-    withEnv([ 'CI = true', 'PUBLIC_URL = %PUBLIC_URL%']) {
-        docker.image('node:12').inside {
-            stage('tests') {
-                withCredentials([string(credentialsId: pipelineParams.token, variable: 'NPMTOKEN')]) {
-                    stage('Install Dependencies') {
-                        sh 'cd client; echo "//registry.npmjs.org/:_authToken=\${NPMTOKEN}" > .npmrc'
-                        sh 'cd client; yarn install'
-                    }
-                    stage('Lint') {
-                        sh 'cd client; yarn lint'
-                    }
-                    stage('Test') {
-                        sh 'cd client; yarn test'
-                    }
-                    stage('Build') {
-                        sh 'cd client; yarn build'
-                    }
-                }
-            }
-        }
+            env.CLEAN_BRANCH_NAME = BRANCH_NAME.replace('/', '_')
+            def customImage = docker.build(registry + "/pipelineParams.repo:${CLEAN_BRANCH_NAME}", './')
+              echo "Successsssss"
+            /* Push the container to DockerHub */
+            //customImage.push()
+          }
+      }
     }
 }
 
-
-
-
-// def call(Map pipelineParams) {
-//     docker.image('node:12').inside {
-//         stage('tests') {
-//             withCredentials([string(credentialsId: pipelineParams.token, variable: 'TOKEN')]) {
-//                 echo "Token: $TOKEN"
-//             }
-//         }
-//     }
-// }
-
-
-
-// def call(Map pipelineParams) {
-//   stage('tests') {
-//      dir(pipelineParams.dir) {
-//        docker.image('node:12').inside {
-//          withCredentials([string(credentialsId: pipelineParams.token, variable: 'token')]) {
-//             echo token
-//          }
-//         }
-//      }
-//   }
-// }  
-
-
-
-
-
-
-// def call(app_name,npm_token) {
-//   // node {
-//   stage('tests') {
-// //     environment {
-// //     NPM_TOKEN = credentials('npm-token')
-// //     CI = 'true'
-// //     PUBLIC_URL = '%PUBLIC_URL%'
-// //    }
-//      dir(app_name) {
-//        docker.image('node:12').inside {
-//          withCredentials([string(credentialsId: npm_token, usernameVariable: 'NPM_TOKEN')]) {
-//             echo NPM_TOKEN
-//              //echo app_name
-//             // sh 'yarn install'
-//          }
-//         }
-//      }
-//   //}
-// }     
-
-
-// def call(directory) {
-//    stages {
-//      stage('test') {
-//         steps {
-//           echo directory
-//         }
-//      }
-//    }
-// }
