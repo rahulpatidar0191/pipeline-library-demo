2f46399a61145bbb84046e984c8947bdd9c7cc6f
Rahul <42219389+rahulpatidar0191@users.noreply.github.com>
2021-06-09 12:02:31 -0700

    Update test.groovy

--- vars/test.groovy
+++ vars/test.groovy
@@ -1,32 +1,42 @@
-
 def server(Map pipelineParams) {
     node {
       stage('Checkout'){
         checkout scm
       }
-
-      stage('Build Docker') {
-          echo 'Building docker image ...'
-          def registry = 'docker.satel.ca'
-          env.REGISTRY = 'docker.satel.ca'
-          def credentials = pipelineParams.credentials
-          echo credentials
-          if (env.TAG_NAME != null) {
-            registry = pipelineParams.registry
-            env.REGISTRY = pipelineParams.registry
-            credentials = pipelineParams.credentials1
-           
-            }
-          // withCredentials([string(credentialsId: pipelineParams.credentials, variable: 'credentials')]) {
-               echo credentials
-              //docker.withRegistry('https://' + registry, credentials) {
-                //env.CLEAN_BRANCH_NAME = BRANCH_NAME.replace('/', '_')
-                //def customImage = docker.build(registry + "/pipelineParams.repo:${CLEAN_BRANCH_NAME}", './')
-                /* Push the container to DockerHub */
-                //customImage.push()
-             // }
-          // }
+     stage('Build Docker') {
+        echo pipelineParams.dockerfiles
       }
     }
 }
+
+// def server(Map pipelineParams) {
+//     node {
+//       stage('Checkout'){
+//         checkout scm
+//       }
+
+//       stage('Build Docker') {
+//           echo 'Building docker image ...'
+//           def registry = 'docker.satel.ca'
+//           env.REGISTRY = 'docker.satel.ca'
+//           def credentials = pipelineParams.credentials
+//           echo credentials
+//           if (env.TAG_NAME != null) {
+//             registry = pipelineParams.registry
+//             env.REGISTRY = pipelineParams.registry
+//             credentials = pipelineParams.credentials1
+           
+//             }
+//           // withCredentials([string(credentialsId: pipelineParams.credentials, variable: 'credentials')]) {
+//                echo credentials
+//               //docker.withRegistry('https://' + registry, credentials) {
+//                 //env.CLEAN_BRANCH_NAME = BRANCH_NAME.replace('/', '_')
+//                 //def customImage = docker.build(registry + "/pipelineParams.repo:${CLEAN_BRANCH_NAME}", './')
+//                 /* Push the container to DockerHub */
+//                 //customImage.push()
+//              // }
+//           // }
+//       }
+//     }
+// }
 
