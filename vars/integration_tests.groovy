3322e67407ea835e4f1349f802f5a5203bf52412
Rahul <42219389+rahulpatidar0191@users.noreply.github.com>
2021-06-08 19:25:09 -0700

    Update integration_tests.groovy

--- vars/integration_tests.groovy
+++ vars/integration_tests.groovy
@@ -1,12 +1,39 @@
 def call(Map pipelineParams) {
-    docker.image('node:12').inside {
-        stage('tests') {
-            withCredentials([string(credentialsId: pipelineParams.token, variable: 'token')]) {
-                echo token
+    withEnv([ 'CI = true', 'PUBLIC_URL = %PUBLIC_URL%']) {
+        docker.image('node:12').inside {
+            stage('tests') {
+                withCredentials([string(credentialsId: pipelineParams.npm_token, NPM_TOKEN: 'npm_token')]) {
+                    stage('Install Dependencies') {
+                        sh 'cd client; echo "//registry.npmjs.org/:_authToken=\${NPM_TOKEN}" > .npmrc'
+                        sh 'cd client; yarn install'
+                    }
+                    stage('Lint') {
+                        sh 'cd client; yarn lint'
+                    }
+                    stage('Test') {
+                        sh 'cd client; yarn test'
+                    }
+                    stage('Build') {
+                        sh 'cd client; yarn build'
+                    }
+                }
             }
         }
     }
 }
+
+
+
+
+// def call(Map pipelineParams) {
+//     docker.image('node:12').inside {
+//         stage('tests') {
+//             withCredentials([string(credentialsId: pipelineParams.token, variable: 'token')]) {
+//                 echo token
+//             }
+//         }
+//     }
+// }
 
 
 
