15513804ad9512f3f6e491381ee7a17d3cff2368
Rahul <42219389+rahulpatidar0191@users.noreply.github.com>
2021-06-09 13:04:58 -0700

    Update build_webapp.groovy

--- vars/build_webapp.groovy
+++ vars/build_webapp.groovy
@@ -4,9 +4,9 @@ def server(Map pipelineParams) {
         checkout scm
       }
      stage('Build Docker') {
-         echo pipelineParams.dockerfiles[1]
+         echo '$pipelineParams.dockerfiles[1]'
         sh "echo 'docker-compose -f $pipelineParams.dockerfiles[1] -f $pipelineParams.dockerfiles[2] up -d'"
-        echo pipelineParams.dockerfiles[0]
+       // echo pipelineParams.dockerfiles[0]
       }
     }
 }
