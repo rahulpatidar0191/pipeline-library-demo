16273b6f93e3706c0f8516816b306e4b390fcf86
Rahul <42219389+rahulpatidar0191@users.noreply.github.com>
2021-06-09 14:30:57 -0700

    Update build_webapp.groovy

--- vars/build_webapp.groovy
+++ vars/build_webapp.groovy
@@ -1,48 +1,51 @@
-node {
-  stage('Checkout'){
-    checkout scm
-  }
-
-  stage('Build Docker') {
-      echo 'Building docker image ...'
-      def registry = pipelineParams.production_registry_host_satel
-      env.REGISTRY = pipelineParams.production_registry_host_satel
-      def credentials = pipelineParams.production_registry_creds_satel
-      if (env.TAG_NAME != null) {
-        registry = pipelineParams.production_registry_host_client
-        env.REGISTRY = pipelineParams.production_registry_host_client
-        credentials = pipelineParams.production_registry_creds_client
+def server(Map pipelineParams) {    
+   node {
+      stage('Checkout'){
+        checkout scm
       }
-      docker.withRegistry('https://' + registry, credentials) {
 
-        env.CLEAN_BRANCH_NAME = BRANCH_NAME.replace('/', '_')
-        def customImage = docker.build(registry + "/${pipelineParams.appname}:${CLEAN_BRANCH_NAME}", './')
-        /* Push the container to DockerHub */
-        customImage.push()
+      stage('Build Docker') {
+          echo 'Building docker image ...'
+          def registry = pipelineParams.production_registry_host_satel
+          env.REGISTRY = pipelineParams.production_registry_host_satel
+          def credentials = pipelineParams.production_registry_creds_satel
+          if (env.TAG_NAME != null) {
+            registry = pipelineParams.production_registry_host_client
+            env.REGISTRY = pipelineParams.production_registry_host_client
+            credentials = pipelineParams.production_registry_creds_client
+          }
+          docker.withRegistry('https://' + registry, credentials) {
+
+            env.CLEAN_BRANCH_NAME = BRANCH_NAME.replace('/', '_')
+            def customImage = docker.build(registry + "/${pipelineParams.appname}:${CLEAN_BRANCH_NAME}", './')
+            /* Push the container to DockerHub */
+            customImage.push()
+          }
       }
-  }
-  try {
-    stage('Docker up') {
-      sh 'docker-compose -f ${pipelineParams.dockerfiles[1]} -f ${pipelineParams.dockerfiles[2]} up -d'
+      try {
+        stage('Docker up') {
+          sh 'docker-compose -f ${pipelineParams.dockerfiles[1]} -f ${pipelineParams.dockerfiles[2]} up -d'
+        }
+        stage('Linting check') {
+          sh 'docker-compose exec -T webapp flake8'
+        }
+        stage('Reports clean up') {
+          sh 'rm -f unittesting.xml coverage.xml typing.xml'
+        }
+        stage('Code tests') {
+          sh 'jenkinsscripts/code-tests.sh'
+        }
+      }
+      catch (exc) {
+        echo 'The validation failed'
+        currentBuild.result = 'FAILURE'
+        if (BRANCH_NAME == 'main') {
+          slackSend channel: '#shape-pim', message: '@here The latest Jenkins job on the main branch of the Shape PIM FAILED.', teamDomain: 'satel', tokenCredentialId: 'slack-token', username: 'Jenkins'
+        }
+      }
     }
-    stage('Linting check') {
-      sh 'docker-compose exec -T webapp flake8'
-    }
-    stage('Reports clean up') {
-      sh 'rm -f unittesting.xml coverage.xml typing.xml'
-    }
-    stage('Code tests') {
-      sh 'jenkinsscripts/code-tests.sh'
-    }
-  }
-  catch (exc) {
-    echo 'The validation failed'
-    currentBuild.result = 'FAILURE'
-    if (BRANCH_NAME == 'main') {
-      slackSend channel: '#shape-pim', message: '@here The latest Jenkins job on the main branch of the Shape PIM FAILED.', teamDomain: 'satel', tokenCredentialId: 'slack-token', username: 'Jenkins'
-    }
-  }
 }
+
 // def server(Map pipelineParams) {
 //     node {
 //       stage('Checkout'){
