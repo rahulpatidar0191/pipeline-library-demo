4261af7b0683aaf13a897b7f89b5e3be0d5ab906
Rahul <42219389+rahulpatidar0191@users.noreply.github.com>
2021-06-09 10:10:47 -0700

    Update test.groovy

--- vars/test.groovy
+++ vars/test.groovy
@@ -11,21 +11,21 @@ def server(Map pipelineParams) {
           env.REGISTRY = 'docker.satel.ca'
           echo registry
           def credentials = pipelineParams.credentials
-          if (env.TAG_NAME != null) {
-            registry = pipelineParams.registry
-            env.REGISTRY = pipelineParams.registry
-            credentials = pipelineParams.credentials1
-              echo 'registry'
-              echo 'credentials'
-              
-          }
-          docker.withRegistry('https://' + registry, credentials) {
-            env.CLEAN_BRANCH_NAME = BRANCH_NAME.replace('/', '_')
-            def customImage = docker.build(registry + "/pipelineParams.repo:${CLEAN_BRANCH_NAME}", './')
-              echo "Successsssss"
-            /* Push the container to DockerHub */
-            //customImage.push()
-          }
+          echo credentials
+//           if (env.TAG_NAME != null) {
+//             registry = pipelineParams.registry
+//             env.REGISTRY = pipelineParams.registry
+//             credentials = pipelineParams.credentials1
+//               echo 'registry'
+//               echo 'credentials'             
+//             }
+//           docker.withRegistry('https://' + registry, credentials) {
+//             env.CLEAN_BRANCH_NAME = BRANCH_NAME.replace('/', '_')
+//             def customImage = docker.build(registry + "/pipelineParams.repo:${CLEAN_BRANCH_NAME}", './')
+//               echo "Successsssss"
+//             /* Push the container to DockerHub */
+//             //customImage.push()
+//           }
       }
     }
 }
