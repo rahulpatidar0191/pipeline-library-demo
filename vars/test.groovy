5062686ba14bf1bb4322477dfcb323f9d081abe9
Rahul <42219389+rahulpatidar0191@users.noreply.github.com>
2021-06-08 21:31:54 -0700

    Update test.groovy

--- vars/test.groovy
+++ vars/test.groovy
@@ -14,15 +14,17 @@ def server(Map pipelineParams) {
             registry = pipelineParams.registry
             env.REGISTRY = pipelineParams.registry
             credentials = pipelineParams.credentials
+              echo 'registry'
+              echo 'credentials'
+              
           }
-          docker.withRegistry('https://' + registry, credentials) {
-
-            env.CLEAN_BRANCH_NAME = BRANCH_NAME.replace('/', '_')
-            def customImage = docker.build(registry + "/pipelineParams.repo:${CLEAN_BRANCH_NAME}", './')
-              echo "Successsssss"
-            /* Push the container to DockerHub */
-            //customImage.push()
-          }
+//           docker.withRegistry('https://' + registry, credentials) {
+//             env.CLEAN_BRANCH_NAME = BRANCH_NAME.replace('/', '_')
+//             def customImage = docker.build(registry + "/pipelineParams.repo:${CLEAN_BRANCH_NAME}", './')
+//               echo "Successsssss"
+//             /* Push the container to DockerHub */
+//             //customImage.push()
+//           }
       }
     }
 }
