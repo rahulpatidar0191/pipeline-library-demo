bb3b440e83bac7fa78a863038fdf7a67aac1176d
Rahul <42219389+rahulpatidar0191@users.noreply.github.com>
2021-06-16 11:47:01 -0700

    Update build_webapp.groovy

--- vars/build_webapp.groovy
+++ vars/build_webapp.groovy
@@ -6,6 +6,24 @@ def server(Map pipelineParams) {
 
       stage('Build Docker') {
           echo 'Building docker image ...'
+         sh '''
+                 echo "Catch the exit codes so we don't exit the whole script before we are done."
+
+                docker-compose exec -T webapp mypy . --junit-xml typing.xml; STATUS1=$?
+
+                docker cp "$(docker-compose ps -q webapp)":/python/app/typing.xml typing.xml
+
+               # Unit and integration testing
+                docker-compose exec -T webapp ./RunTest.sh; STATUS2=$?
+
+                docker cp "$(docker-compose ps -q webapp)":/python/app/unittesting.xml unittesting.xml
+               docker cp "$(docker-compose ps -q webapp)":/python/app/coverage.xml coverage.xml
+
+               # Return the status code of mypy
+               TOTAL=$((STATUS1 + STATUS2))
+
+               exit $TOTAL
+          '''
   
           }
       }
