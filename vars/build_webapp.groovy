d9f6b5a3e5af1b051d7029e04d4bb7dfecadbc3c
Rahul <42219389+rahulpatidar0191@users.noreply.github.com>
2021-06-16 11:19:18 -0700

    add multiline

--- vars/build_webapp.groovy
+++ vars/build_webapp.groovy
@@ -4,38 +4,32 @@ def server(Map pipelineParams) {
         checkout scm
       }
 
-//       stage('Build Docker') {
-//           echo 'Building docker image ...'
-//           def registry = pipelineParams.production_registry_host_satel
-//           env.REGISTRY = pipelineParams.production_registry_host_satel
-//           def credentials = pipelineParams.production_registry_creds_satel
-//           if (env.TAG_NAME != null) {
-//             registry = pipelineParams.production_registry_host_client
-//             env.REGISTRY = pipelineParams.production_registry_host_client
-//             credentials = pipelineParams.production_registry_creds_client
-//           }
-//           docker.withRegistry('https://' + registry, credentials) {
 
-//             env.CLEAN_BRANCH_NAME = BRANCH_NAME.replace('/', '_')
-//             def customImage = docker.build(registry + "/${pipelineParams.appname}:${CLEAN_BRANCH_NAME}", './')
-//             /* Push the container to DockerHub */
-//             customImage.push()
-//           }
-//       }
       try {
         stage('Docker up') {
           //sh "docker-compose -f ${pipelineParams.dockerfiles[1]} -f ${pipelineParams.dockerfiles[2]} up -d"
            echo "docker stack deploy --with-registry-auth -c ${pipelineParams.dockerfiles[3]} ${pipelineParams.appName}"
            
-        }
-//         stage('Linting check') {
-//           sh 'docker-compose exec -T webapp flake8'
-//         }
-//         stage('Reports clean up') {
-//           sh 'rm -f unittesting.xml coverage.xml typing.xml'
-//         }
         stage('Code tests') {
-          sh 'jenkinsscripts/code-tests.sh'
+          sh '''
+                # Catch the exit codes so we don't exit the whole script before we are done.
+
+               # Typing check
+               docker-compose exec -T webapp mypy . --junit-xml typing.xml; STATUS1=$?
+
+               docker cp "$(docker-compose ps -q webapp)":/python/app/typing.xml typing.xml
+
+               # Unit and integration testing
+               docker-compose exec -T webapp ./RunTest.sh; STATUS2=$?
+
+               docker cp "$(docker-compose ps -q webapp)":/python/app/unittesting.xml unittesting.xml
+               docker cp "$(docker-compose ps -q webapp)":/python/app/coverage.xml coverage.xml
+
+               # Return the status code of mypy
+               TOTAL=$((STATUS1 + STATUS2))
+
+               exit $TOTAL
+         '''
         }
       }
       catch (exc) {
