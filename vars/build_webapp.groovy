5429412070d790876965562cae6ccadc5910de2e
Rahul <42219389+rahulpatidar0191@users.noreply.github.com>
2021-06-16 11:40:26 -0700

    Update build_webapp.groovy

--- vars/build_webapp.groovy
+++ vars/build_webapp.groovy
@@ -1,30 +1,33 @@
 
 def call() {    
    node {
-      stage('Checkout'){
-        checkout scm
+//       stage('Checkout'){
+//         checkout scm
+//       }
+      stage('chck'){
+            echo " rahul"
       }
-       stage('Code tests') {
-          sh '''
-                echo "Catch the exit codes so we don't exit the whole script before we are done."
+//        stage('Code tests') {
+//           sh '''
+//                 echo "Catch the exit codes so we don't exit the whole script before we are done."
 
-               # Typing check
-               docker-compose exec -T webapp mypy . --junit-xml typing.xml; STATUS1=$?
+//                # Typing check
+//                docker-compose exec -T webapp mypy . --junit-xml typing.xml; STATUS1=$?
 
-               docker cp "$(docker-compose ps -q webapp)":/python/app/typing.xml typing.xml
+//                docker cp "$(docker-compose ps -q webapp)":/python/app/typing.xml typing.xml
 
-               # Unit and integration testing
-               docker-compose exec -T webapp ./RunTest.sh; STATUS2=$?
+//                # Unit and integration testing
+//                docker-compose exec -T webapp ./RunTest.sh; STATUS2=$?
 
-               docker cp "$(docker-compose ps -q webapp)":/python/app/unittesting.xml unittesting.xml
-               docker cp "$(docker-compose ps -q webapp)":/python/app/coverage.xml coverage.xml
+//                docker cp "$(docker-compose ps -q webapp)":/python/app/unittesting.xml unittesting.xml
+//                docker cp "$(docker-compose ps -q webapp)":/python/app/coverage.xml coverage.xml
 
-               # Return the status code of mypy
-               TOTAL=$((STATUS1 + STATUS2))
+//                # Return the status code of mypy
+//                TOTAL=$((STATUS1 + STATUS2))
 
-               exit $TOTAL
-         '''
-        }
+//                exit $TOTAL
+//          '''
+//         }
    }
 }
 
